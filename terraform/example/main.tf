d329ac1016fd3bdd597e5538e978ad725f2111af
kohei789 <noguchi.kohei789@gmail.com>
2023-07-19 17:15:04 +0900

    検証用(grafanaなしで構築してみる)

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -5,4 +5,5 @@ module "fourkeys" {
   region              = var.region
   bigquery_region     = var.bigquery_region
   parsers             = var.parsers
+  enable_dashboard    = var.enable_dashboard
 }
--- terraform/example/variables.tf
+++ terraform/example/variables.tf
@@ -19,15 +19,21 @@ variable "bigquery_region" {
   type        = string
   default     = "US"
   description = "Region to deploy BigQuery resources in."
-  validation {
-    condition     = can(regex("^(US|EU)$", var.bigquery_region))
-    error_message = "The value for 'bigquery_region' must be one of: 'US','EU'."
-  }
+#  validation {
+#    condition     = can(regex("^(US|EU)$", var.bigquery_region))
+#    error_message = "The value for 'bigquery_region' must be one of: 'US','EU'."
+#  }
 }
 
 variable "parsers" {
   type        = list(string)
   description = "List of data parsers to configure. Acceptable values are: 'github', 'gitlab', 'cloud-build', 'tekton', 'pagerduty'"
+}
+
+variable "enable_dashboard" {
+  type        = bool
+  description = "enable dashboard"
+  default     = false
 }
 
 variable "event_handler_container_url" {
